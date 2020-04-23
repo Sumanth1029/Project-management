9de33170c8f9676117a122c583b9695b1bc306c0
Sumanth Hegde <sumanthhegde1029@gmail.com>
2020-04-24 00:15:35 +0530

    some basic components

--- react_part/src/App.js
+++ react_part/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch,Route } from "react-router-dom";
 import Navbar from "./components/layouts/navbar";
+import Dashboard from "./components/dashboard/dashboard"
+import ProjectDetails from "./components/projects/project_details"
+
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar/>
+        <Switch>
+          <Route exact path="/project/:id" component={ProjectDetails}/>
+          <Route path="/" component={Dashboard}/>
+
+        </Switch>
       </div>
     </BrowserRouter>
   );
---
+++ react_part/src/components/projects/project_summary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ProjectList from './project_list'
+
+const ProjectSummary=()=>{
+    return(
+        <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project title</span>
+                    <p>Posted by sumanth</p>
+                    <p className="grey-text">3rd September, 2AM</p>
+                </div>
+            </div>
+    )
+}
+
+export default ProjectSummary
