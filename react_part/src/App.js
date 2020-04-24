8be2a7b9c2d0a60b77c73377514d302d32d03358
Sumanth Hegde <sumanthhegde1029@gmail.com>
2020-04-24 09:53:48 +0530

    sign in and sign up pages

--- react_part/src/App.js
+++ react_part/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch,Route } from "react-router-dom";
 import Navbar from "./components/layouts/navbar";
 import Dashboard from "./components/dashboard/dashboard"
 import ProjectDetails from "./components/projects/project_details"
+import SignIn from "./components/auth/sign_in" 
+import SignUp from "./components/auth/sign_up" 
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Navbar/>
         <Switch>
           <Route exact path="/project/:id" component={ProjectDetails}/>
+          <Route exact path="/signin" component={SignIn}/>
+          <Route exact path="/signup" component={SignUp}/>
           <Route path="/" component={Dashboard}/>
 
         </Switch>
