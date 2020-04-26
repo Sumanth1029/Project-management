00498ceeec3043f150eb736c5f048d31be3ca598
Sumanth Hegde <sumanthhegde1029@gmail.com>
2020-04-26 11:32:46 +0530

    connected to firebase for insertion and display operation

--- react_part/src/store/actions/projectActions.js
+++ react_part/src/store/actions/projectActions.js
@@ -1,5 +1,25 @@
-export const createProject=(project)=>{
-    return(dispatch,getState)=>{
-        dispatch({type:"CREATE_PROJECT",project})
-    }
-}
+import { firestore } from "firebase";
+
+export const createProject = (project) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const fireStore = getFirestore();
+    firestore()
+      .collection('projects')
+      .add({
+        ...project,
+        authorFirstName: "Shreedhar",
+        authorLastName: "Hegde",
+        authorId: 12589,
+        createdAt: new Date(),
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT", project });
+      })
+      .catch((err)=>{
+        dispatch({type:'CREATE_PROJECT_ERROR',err})
+      })
+  };
+};
+
+
+////
--- react_part/src/store/reducers/projectReducer.js
+++ react_part/src/store/reducers/projectReducer.js
@@ -1,17 +1,23 @@
+const initState = {
+  projects: [
+    { id: "1", title: "proj1", content: "dfdsfsdf" },
+    { id: "2", title: "proj2", content: "dfdsfsdf" },
+    { id: "3", title: "proj3", content: "dfdsfsdf" },
+  ],
+};
 
-const initState={
-    projects:[
-        {id:'1',title:"proj1",content:"dfdsfsdf"},
-        {id:'2',title:"proj2",content:"dfdsfsdf"},
-        {id:'3',title:"proj3",content:"dfdsfsdf"}
-    ]
-}
+const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "CREATE_PROJECT":
+      console.log("project created", action.project);
+      return state;
 
-const projectReducer=(state=initState,action)=>{
-    switch(action.type){
-        case "CREATE_PROJECT":
-            console.log("project created",action.project)
-    }
-    return state
-}
-export default projectReducer
+    case "CREATE_PROJECT_ERROR":
+      console.log("create project error", action.err);
+      return state;
+
+    default:
+      return state;
+  }
+};
+export default projectReducer;
