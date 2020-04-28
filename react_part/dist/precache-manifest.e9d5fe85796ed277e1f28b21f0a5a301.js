1c64a5df601356acab4e23abbaeb5b71bbf0cda0
Sumanth Hegde <sumanthhegde1029@gmail.com>
2020-04-28 15:23:20 +0530

    deployed

---
+++ react_part/dist/precache-manifest.e9d5fe85796ed277e1f28b21f0a5a301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a78334a24feb21cbb92bfbcd76e9e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2336bdfd2678a7bc8ad4",
+    "url": "/static/css/main.e6dabcd2.chunk.css"
+  },
+  {
+    "revision": "25064000bf2c86037ba5",
+    "url": "/static/js/2.93246a5b.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.93246a5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2336bdfd2678a7bc8ad4",
+    "url": "/static/js/main.425ce1ca.chunk.js"
+  },
+  {
+    "revision": "ae41b1d6ba276b7d2b53",
+    "url": "/static/js/runtime-main.16bb5cdb.js"
+  }
+]);
